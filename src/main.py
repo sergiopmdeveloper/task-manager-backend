4b16e85df8cd9da7ee8f6d915818173492d96568
Sergio <sergio.pm.developer@gmail.com>
2024-01-07 02:29:29 +0000

    feat: add custom CORS middleware to allow requests from the frontend

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import re
+from typing import Awaitable, Callable
+
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
@@ -17,6 +20,45 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def custom_cors_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    """
+    Custom CORS middleware to allow
+    requests from the frontend
+
+    Parameters
+    ----------
+    request : Request
+        The request object
+    call_next : Callable[[Request], Awaitable[Response]]
+        The next function to call
+
+    Returns
+    -------
+    Response
+        The response object
+    """
+
+    if "origin" in request.headers:
+        origin = request.headers["origin"]
+
+        if re.match(r"^https?://task-manager-frontend-.*\.vercel\.app$", origin):
+            response = await call_next(request)
+            response.headers["Access-Control-Allow-Origin"] = origin
+            response.headers[
+                "Access-Control-Allow-Methods"
+            ] = "GET, POST, PUT, DELETE, OPTIONS"
+            response.headers["Access-Control-Allow-Headers"] = "*"
+            response.headers["Access-Control-Allow-Credentials"] = "true"
+
+            return response
+
+    return await call_next(request)
+
 
 app.include_router(auth_router)
 
